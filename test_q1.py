a304219d85439106142d8a2b703d579c595862e2
21020416LeBaTruong <septruong0203@gmail.com>
2024-10-26 23:36:43 +0700

    question-1 update test case

--- test_q1.py
+++ test_q1.py
@@ -6,17 +6,17 @@ class TestQ1(unittest.TestCase):
     def test_1(self):
         self.assertEqual(q1.most_frequent_lengths(['a', 'ab', 'abc', 'cd', 'def', 'gh']), ['ab', 'cd', 'gh'])
 
-    # def test_2(self):
-        # self.assertEqual(q1.most_frequent_lengths(['apple', 'pie', 'banana', 'kiwi']), ['apple', 'banana'])
+    def test_2(self):
+        self.assertEqual(q1.most_frequent_lengths(['apple', 'pie', 'banana', 'kiwi', 'orange']), ['banana', 'orange'])
 
-    # def test_3(self):
-    #     self.assertEqual(q1.most_frequent_lengths(['a', 'aa', 'aaa']), ['a', 'aa', 'aaa'])
+    def test_3(self):
+        self.assertEqual(q1.most_frequent_lengths(['a', 'aa', 'aaa', 'ab', 'lmao', 'ac']), ['aa', 'ab', 'ac'])
 
-    # def test_4(self):
-    #     self.assertEqual(q1.most_frequent_lengths(['hello', 'world', 'python', 'code']), ['hello', 'world', 'python'])
+    def test_4(self):
+        self.assertEqual(q1.most_frequent_lengths(['hello', 'world', 'python', 'code']), ['hello', 'world'])
 
-    # def test_5(self):
-    #     self.assertEqual(q1.most_frequent_lengths(['abc', 'de', 'fghi', 'jk', 'lm', 'nop']), ['de', 'jk', 'lm'])
+    def test_5(self):
+        self.assertEqual(q1.most_frequent_lengths(['one', 'two', 'three', 'four', 'five', 'six']), ['one', 'two', 'six'])
 
 
 if __name__ == '__main__':
